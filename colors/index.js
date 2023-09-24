c8da63104e9437964dc08aaee79176b5d5f8a45c
hubhub555 <zbuhibuhi@gmail.com>
2023-09-24 22:51:40 +0900

    init

---
+++ colors/index.js
@@ -0,0 +1,7 @@
+const colors = require('colors');
+
+const s1 = "A time to throw stones away";
+const s2 = "and atime to gather stones together";
+
+console.log(s1.underline.red);
+console.log(s2.underline.blue);
